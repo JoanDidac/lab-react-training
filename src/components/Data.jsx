01077f3bc77fb11545787d11b56683261d0855f7
Joan D <joandidachr@gmail.comm>
2023-02-27 12:07:31 +0100

    Initial Commit

---
+++ src/components/Data.jsx
@@ -0,0 +1,22 @@
+
+
+const usersArray = [
+    {
+      lastName: 'Hernandez',
+      firstName: 'JoanDi',
+      gender: 'male',
+      height: 1.71,
+      birth: '1987-08-12',
+      picture: 'https://s3.amazonaws.com/gex.lifeandstyle/uploads/image/file/3361/dude2.jpg'
+    },
+    {
+      lastName: 'Delores',
+      firstName: 'Obrien',
+      gender: 'female',
+      height: 1.72,
+      birth: '1988-05-11',
+      picture: 'https://randomuser.me/api/portraits/women/44.jpg'
+    }
+  ];
+
+  export default usersArray;
---
+++ src/components/IdCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+// import usersArray from './Data';
+
+
+const IdCard = (props) => {
+    console.log(props);
+    const {user: {firstName,lastName, gender, height, birth, picture }} = props;
+    
+    
+  
+
+    return (
+      <div className="IdCard">
+        <img className="profile-image" src={picture} alt="ProfilePicture" />
+        <div className="cardDetails">
+          <p>First Name: {firstName}</p>
+          <p>Last Name: {lastName}</p>
+          <p>Gender: {gender}</p>
+          <p>Height: {height}</p>
+          <p>Birthdate: {birth}</p>
+        </div>
+      </div>
+    );
+  };
+
+export default IdCard;
+
