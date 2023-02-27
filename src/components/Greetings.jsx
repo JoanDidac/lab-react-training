8b77a4b0b0ee2eb179b2d997fb2ac54d8646f726
Joan D <joandidachr@gmail.comm>
2023-02-27 12:32:17 +0100

    Second commit

---
+++ src/components/Greetings.jsx
@@ -0,0 +1,38 @@
+
+import IdCard from './components/IdCard';
+import usersArray from './components/Data.jsx';
+
+const Greeting = (props) => {
+
+    const { language, children } = props;
+
+let lang;
+
+    switch (language) {
+        case "de":
+            lang='Hallo';
+        break;
+        case "en":  
+           lang =  "Hello";
+        break;
+        case "sp":
+           lang =  "Hola";
+        break;
+        case "fr":
+            lang = "Bonjour";
+        break;
+         default:
+        break;
+        
+    };
+
+    return (
+        <div className='card-container' >
+            <div className='card'>
+                <h2>{lang} {children}</h2>
+
+            </div>
+
+        </div>
+    )
+}
